b5b39d3da5772994109928f6a02c90c76daa8cf9
jsjune <avraskio@gmail.com>
2022-01-12 10:02:15 +0900

    update

--- src/ByteAdder.java
+++ src/ByteAdder.java
@@ -1,21 +1,33 @@
-
 public class ByteAdder {
 	public boolean[] byteadder(boolean[] byteA, boolean[] byteB) {
 		Gate g = new Gate();
 		boolean[] result = new boolean[9];
-			
-		for(int i = 0; i<result.length-1;i++) {
-			boolean byteC = g.and(byteA[i], byteB[i]);
-			boolean sum = g.xor(byteA[i], byteB[i]);
-			if(i==0) {
-				result[0]=sum;
-			}
-			else if(byteC==true) {
-				result[i+1]=g.xor(g.xor(byteA[i+1], byteB[i+1]),byteC);
-			}
-			else {
-				result[i]=sum;
-			}
+		
+		boolean[] carry = new boolean[9];
+		carry[0] = false;
+		for(int i=0;i<carry.length-1;i++) {
+			carry[i+1] = g.and(byteA[i], byteB[i]);
+		}
+		
+		boolean[] sum = new boolean[9];
+		sum[0] = false;
+		for(int i=0;i<sum.length-1;i++) {
+			sum[i+1] = g.xor(byteA[i], byteB[i]);
+		}
+		
+		boolean[] abcarry = new boolean[9];
+		for(int i=0; i<abcarry.length; i++) {
+			abcarry[i]=g.xor(sum[i], carry[i]);
+		}
+		
+		boolean[] absum = new boolean[9];
+		absum[8] = false;
+		for(int i=0;i<absum.length-1;i++) {
+			absum[i]=g.xor(byteA[i], byteB[i]);
+		}
+		
+		for(int i=1;i<result.length;i++) {
+			result[i]=g.xor(absum[i],abcarry[i]);
 		}
 		return result;
 	}
