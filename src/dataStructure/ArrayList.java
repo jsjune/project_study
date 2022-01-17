d706eeabf47f355d5773626db89e19b601b44770
jsjune <avraskio@gmail.com>
2022-01-17 18:05:22 +0900

    update

--- src/dataStructure/ArrayList.java
+++ src/dataStructure/ArrayList.java
@@ -6,6 +6,10 @@ public class ArrayList {
 	// 타입 Object는 String,Integer와 상속 관계, Object가 최상위 타입이므로 모든 객체는 Object타입으로 자동 타입
 	// 변환될 수 있다.
 
+	public ArrayList() {
+		
+	}
+	
 	public boolean addFirst(Object element) {
 		return add(0, element);
 	}
@@ -64,11 +68,11 @@ public class ArrayList {
 		return elementData[index];
 	}
 
-	public Object size() {
+	public int size() {
 		return size;
 	}
 
-	public Object indexOf(Object o) {
+	public int indexOf(Object o) {
 		for (int i = 0; i < size; i++) {
 			if (o.equals(elementData[i])) { // .equals : 대상의 내용 자체를 비교, == : 대상의 주소값을 비교
 				return i;
@@ -77,6 +81,52 @@ public class ArrayList {
 		return -1;
 	}
 	
+	public ListIterator listIterator() { // 리턴할 데이터 타입, 메소드이름()
+		// ListIterator 인스턴스를 생성해서 리턴합니다.
+		return new ListIterator();
+	}
 	
+	class ListIterator{
+		// 현재 탐색하고 있는 순서를 가리키는 인덱스 값
+		private int nextIndex = 0;
+		
+		// next 메소드를 호출할 수 있는지를 체크한다.
+		public boolean hasNext() {
+			// nextIndex가 엘리먼트의 숫자보다 적다면 next를 이용해서 탐색할 엘리먼트가 존재하는 것이기 때문에 true를 리턴한다.
+			return nextIndex < size();
+		}
+		
+		// 순차적으로 엘리먼트를 탐색해서 리턴한다.
+		public Object next() {
+//			Object returnData = elementData[nextIndex];
+//			nextIndex++;
+//			return returnData;
+			// nextIndex에 해당하는 엘리먼트를 리턴하고 nextIndex의 값을 1 증가 시킵니다.
+			return elementData[nextIndex++];
+		}
+		
+		// previous 메소드를 호출해도 되는지를 체크
+		public boolean hasPrevious() {
+			// nextIndex가 0보다 크다면 이전 엘리먼트가 존재한다는 의미
+			return nextIndex > 0;
+		}
+
+		// 순차적으로 이전 노드를 리턴
+		public Object previous() {
+			// 이전 엘리먼트를 리턴하고 nextIndex의 값을 1감소합니다.
+			return elementData[--nextIndex];
+		}
+		
+		// 현재 엘리먼트를 추가합니다.
+		public void add(Object element) {
+			ArrayList.this.add(nextIndex++, element);
+		}
+		
+		// 현재 엘리먼트를 삭제합니다.
+		public void remove() { // next를 최소한 한번 호출한 후에 삭제해야한다.
+			ArrayList.this.remove(nextIndex-1);
+			nextIndex--;
+		}
+	}
 	
 }
--- src/dataStructure/Main.java
+++ src/dataStructure/Main.java
@@ -4,24 +4,54 @@ public class Main {
 
 	public static void main(String[] args) {
 
-			ArrayList numbers = new ArrayList();
-			
-			numbers.addLast(10);
-			numbers.addLast(20);
-			numbers.addLast(30);
-			numbers.addLast(40);
-			numbers.add(1, 15);
-			numbers.addFirst(5);
-			System.out.println(numbers);
-			System.out.println(numbers.remove(1));
-			System.out.println(numbers);
-			numbers.removeFirst();
-			System.out.println(numbers);
-			numbers.removeLast();
-			System.out.println(numbers);
-			System.out.println(numbers.get(1));
-			System.out.println(numbers.size());
-			System.out.println(numbers.indexOf(20));
-	}
+		ArrayList numbers = new ArrayList();
 
+		numbers.addLast(10);
+		numbers.addLast(20);
+		numbers.addLast(30);
+		numbers.addLast(40);
+		numbers.add(1, 15);
+		numbers.addFirst(5);
+		System.out.println(numbers);
+		System.out.println(numbers.remove(1));
+		System.out.println(numbers);
+		numbers.removeFirst();
+		System.out.println(numbers);
+		numbers.removeLast();
+		System.out.println(numbers);
+		System.out.println(numbers.get(1));
+		System.out.println(numbers.size());
+		System.out.println(numbers.indexOf(20));
+		numbers.addLast(50);
+		
+		ArrayList.ListIterator li = numbers.listIterator();
+		while(li.hasNext()) {
+			System.out.println(li.next());
+			
+		}
+		
+		while(li.hasPrevious()) {
+			System.out.println(li.previous());
+		}
+		
+		while(li.hasNext()) {
+			int number = (int)li.next();
+			if(number == 30) {
+				li.add(35);
+			}
+		}
+		System.out.println(numbers);
+		
+		while(li.hasPrevious()) {
+			System.out.println(li.previous());
+		}
+		
+		while(li.hasNext()) {
+			int number = (int)li.next();
+			if(number == 30) {
+				li.remove();
+			}
+		}
+		System.out.println(numbers);
+	}
 }
