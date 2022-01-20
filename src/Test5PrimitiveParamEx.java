3f07bf44874585325b9ffb67704d603b047cc123
jsjune <avraskio@gmail.com>
2022-01-20 20:55:32 +0900

    initial commit

--- src/Test5PrimitiveParamEx.java
+++ src/Test5PrimitiveParamEx.java
@@ -1,20 +1,22 @@
 // 기본형 매개변수 : 변수의 값을 읽기만 할 수 있다.
-class Data{int x;}
+class Data {
+	int x;
+}
 
 public class Test5PrimitiveParamEx {
 	public static void main(String[] args) {
 		Data d = new Data();
-		d.x=10;
-		System.out.println("main() : x = "+d.x);
-		
+		d.x = 10;
+		System.out.println("main() : x = " + d.x);
+
 		change(d.x);
 		System.out.println("After change(d.x)");
-		System.out.println("main() : x = "+d.x);
-		
+		System.out.println("main() : x = " + d.x);
+
 	}
-	
+
 	static void change(int x) {
-		x=1000;
-		System.out.println("change() : x = "+x);
+		x = 1000;
+		System.out.println("change() : x = " + x);
 	}
 }
--- src/Test5ReferenceParamEx.java
+++ src/Test5ReferenceParamEx.java
@@ -8,11 +8,11 @@ public class Test5ReferenceParamEx {
 		change(d);
 		System.out.println("After change(d)");
 		System.out.println("main() : x = " + d.x);
-		
+
 	}
-	
+
 	static void change(Data d) {
-		d.x =1000;
+		d.x = 1000;
 		System.out.println("change() : x = " + d.x);
 	}
 }
